b50ef1f707547768f1848f8c496ae9104fa3d06e
Murat Shadol <shadolmurat@gmail.com>
2023-12-17 15:00:20 +0300

    Add lengthOfLongestSubstring

--- Medium/lengthOfLongestSubstring.py
+++ Medium/lengthOfLongestSubstring.py
@@ -5,25 +5,28 @@ without repeating characters.
 """
 
 def lengthOfLongestSubstring(s):
-    letters = {}
-    length = 0
-    current_length = 0
-    for i in range(len(s)):
-        if letters.get(s[i]):
-            if current_length > length:
-                length = current_length
-            current_length = 0
-            letters = {}
+    chs = set()
+    start = 0
+    end = 0
+    max_ch = 0
+    while end < len(s):
+        while s[end] in chs:
+            chs.remove(s[start])
+            start += 1
+        chs.add(s[end])
+        max_ch = max(end-start+1, max_ch)
+        end += 1
+    return max_ch
 
-        letters[s[i]] = True
-        current_length += 1
-    if current_length > length:
-        length = current_length
-    if s == "":
-        return 0
-    elif len(s) == 1:
-        return 1
-    else:
-        return length
 
-print(lengthOfLongestSubstring("dvdf"))
+assert lengthOfLongestSubstring("dvdf") == 3
+assert lengthOfLongestSubstring("abcabcbb") == 3
+assert lengthOfLongestSubstring("pwwkew") == 3
+assert lengthOfLongestSubstring("bbbbb") == 1
+
+"""
+Пройтись по строке, на каждом шаге запускать цикл,
+который будет убирать из сета все вхождения текущего элемента в сет слева.
+После цикла добавить элемент в сет, вычислить максимум между текущим значением
+окна (подстроки) и существующим.
+"""
